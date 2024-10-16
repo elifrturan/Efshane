ae5550d52481d9d8f79a8cc4d13e75685e376e77
elifr <elifrturanceng@gmail.com>
2024-10-17 00:12:17 +0300

    update emailconfirm page

--- src/pages/password/EmailConfirm.jsx
+++ src/pages/password/EmailConfirm.jsx
@@ -5,16 +5,13 @@ import { Link } from 'react-router-dom'
 function EmailConfirm() {
   return (
     <div className="email-container">
-      <div className="row mt-0  mb-3 arrowLeft">
-        <Link to='/signin'><i class="bi bi-arrow-left-circle-fill"></i></Link>
-      </div>
       <h2 className='text-center text-white'>Şifrenizi Yenileyin</h2>
       <p className='text-white text-center opacity-50'>E-posta adresinizi girerek şifre yenileme talebinde bulunun. 
         Size gönderilecek doğrulama koduyla şifrenizi kolayca sıfırlayabilirsiniz.
       </p>
       <form>
         <input type="email" name='email' id='email' className='form-control bg-transparent text-white mb-3' placeholder='Email'/>
-        <Link className='btn btn-confirm fw-bold' to='/emailconfirm/codeverification'>Şifre Sıfırlama E-Postası Gönder</Link>
+        <Link className='btn btn-confirm' to='/emailconfirm/codeverification'>Şifre Sıfırlama E-Postası Gönder</Link>
       </form>
     </div>
   )
