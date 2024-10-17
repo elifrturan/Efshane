a2c9a0e02960d70a1363605e8c439950a2df6c48
elifr <elifrturanceng@gmail.com>
2024-10-17 14:27:43 +0300

    code verify page done

--- src/pages/password/CodeVerification.jsx
+++ src/pages/password/CodeVerification.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './CodeVerification.css'
+import { Link } from 'react-router-dom'
 
 function CodeVerification() {
   return (
-    <div>CodeVerification</div>
+    <div className="code-container">
+      <h2 className='text-center text-white'>Doğrulama Kodu Girin</h2>
+      <p className='text-center text-white opacity-50'>
+        E-posta adresinize gönderilen doğrulama kodunu aşağıya girin. 
+        Bu adımı tamamladıktan sonra şifrenizi sıfırlayabileceksiniz.
+      </p>
+      <div className='d-flex justify-content-center align-items-center mb-3'>
+        <div className="d-flex gap-2 box">
+          <input type="text" maxLength="1" className='form-control text-center bg-transparent text-white me-2'/>
+          <input type="text" maxLength="1" className='form-control text-center bg-transparent text-white me-2'/>
+          <input type="text" maxLength="1" className='form-control text-center bg-transparent text-white me-2'/>
+          <input type="text" maxLength="1" className='form-control text-center bg-transparent text-white me-2'/>
+          <input type="text" maxLength="1" className='form-control text-center bg-transparent text-white'/>
+        </div>
+      </div>
+      <Link className='btn btn-verify text-center' to="/emailconfirm/codeverification/passwordrenewal">E-Posta Doğrula</Link>
+    </div>
   )
 }
 
