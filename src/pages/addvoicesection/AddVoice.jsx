1c25ee910212c1d950eee56a394a74c30097570c
elifr <elifrturanceng@gmail.com>
2025-01-09 12:44:07 +0300

    add cover update

--- src/pages/addvoicesection/AddVoice.jsx
+++ src/pages/addvoicesection/AddVoice.jsx
@@ -40,6 +40,45 @@ function AddVoice() {
         console.log("yeni bölüm eklemek istersen");
     }
 
+    const [bookImage, setImage] = useState("");
+    const [error, setError] = useState("");
+
+    const handleImageUpload = async (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const img = new Image();
+            img.onload = async () => {
+                if (img.width > 200 || img.height > 281) {
+                    setError("Görsel boyutu 200x281 pikselden büyük olamaz.");
+                    setTimeout(() => setError(""), 5000);
+                } else {
+                    setError("");
+                    const reader = new FileReader();
+                    reader.onloadend = () => {
+                        setImage(reader.result);
+                    };
+                    reader.readAsDataURL(file);
+                    try {
+                        const base64Image = await convertToBase64(file); 
+                        setImage(base64Image);
+                    } catch (error) {
+                        console.error("Görsel dönüştürme hatası:", error);
+                    }
+                }
+            };
+            img.src = URL.createObjectURL(file);
+        }
+    };
+    
+    const convertToBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (error) => reject(error);
+        });
+    };
+
     return (
         <div className="add-voice-section-page">
             <div className="container">
@@ -47,7 +86,23 @@ function AddVoice() {
                 <div className="add-voice-section-main">
                     <div className="add-voice-section-left">
                         {/* Upload Cover */}
-                    <AddCover/>
+                        {bookImage ? (
+                            <img src={bookImage} alt="uploaded" width="200px" height="281px" />
+                            ) : (
+                            <img src="/images/upload-image.svg" alt="upload-image" width="200px" height="281px" />
+                        )}
+                        <input
+                            type="file"
+                            accept="image/*"
+                            id="image-upload"
+                            style={{ display: 'none' }}
+                            onChange={handleImageUpload}
+                        />
+                        <button onClick={() => document.getElementById('image-upload').click()}>Görsel Yükle <i className="bi bi-cloud-arrow-up-fill ms-1"></i></button>
+                        {error && <p className='error-message-cover'>{error}</p>}
+                        <i className='left-description'>Kitabınız kapağını değiştirmek istiyorsanız
+                            görsel yükle butonuna tıklamanız yeterli olacaktır.
+                        </i>
                     </div>
                     <div className="add-voice-section-right">
                     {/* Tabs */}
@@ -65,7 +120,7 @@ function AddVoice() {
                                 Bölümler
                             </button>
                         </div>
-                    {/* Content */}
+                        {/* Content */}
                         <div className="voice-tab-content">
                             {/* Details */}
                             {activeTab === 'details' && <Details/>}
